5c0cf88ccee8105c7b7ee3f3d38286c60537505f
didipta <66532706+didipta@users.noreply.github.com>
2022-11-08 18:19:58 +0600

    limit add service

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//async await 
-// async function run(){
 
-// }
-//user:
-//pass:
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jpoqt1r.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,9 +20,22 @@ async function run(){
 try{
  const serviceCollection=client.db('Wedding_Photography').collection("Services");
  app.get('/services',async(req,res)=>{
+    const size=req.query.size;
     const query={};
-    const cursor=serviceCollection.find(query);
-    const service =await cursor.toArray();
+    
+    let service
+    if(size==3)
+    {
+        const cursor=serviceCollection.find(query);
+        service =await cursor.limit(3).toArray();
+        console.log(size);
+    }
+    else
+    {
+        const cursor=serviceCollection.find(query);
+        service =await cursor.toArray();
+    }
+    
     res.send(service);
  })
 }
